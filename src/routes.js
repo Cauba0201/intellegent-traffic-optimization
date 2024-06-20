0ab653122e1f7c21d17391eff530b7b4a4a39f9b
Cauba0201 <91552618+Cauba0201@users.noreply.github.com>
2024-06-20 09:43:21 +0700

    Add feature for sidebar

--- src/routes.js
+++ src/routes.js
@@ -24,50 +24,113 @@ const routes = [
     name: "Main Dashboard",
     layout: "/admin",
     path: "/default",
-    icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
+    icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
     component: MainDashboard,
   },
   {
-    name: "NFT Marketplace",
+    name: "Signal & Quality",
     layout: "/admin",
     path: "/nft-marketplace",
     icon: (
       <Icon
         as={MdOutlineShoppingCart}
-        width='20px'
-        height='20px'
-        color='inherit'
+        width="20px"
+        height="20px"
+        color="inherit"
       />
     ),
     component: NFTMarketplace,
     secondary: true,
   },
   {
-    name: "Data Tables",
+    name: "Signal Quality",
     layout: "/admin",
-    icon: <Icon as={MdBarChart} width='20px' height='20px' color='inherit' />,
+    icon: <Icon as={MdBarChart} width="20px" height="20px" color="inherit" />,
     path: "/data-tables",
     component: DataTables,
+  },
+  {
+    name: "Data Tables",
+    layout: "/admin",
+    icon: <Icon as={MdBarChart} width="20px" height="20px" color="inherit" />,
+    path: "/data-tables",
+    component: DataTables,
+  },
+  {
+    name: "Phone calls",
+    layout: "/admin",
+    path: "/profile",
+    icon: <Icon as={MdPerson} width="20px" height="20px" color="inherit" />,
+    component: Profile,
+  },
+  {
+    name: "Data traffic",
+    layout: "/admin",
+    path: "/profile",
+    icon: <Icon as={MdPerson} width="20px" height="20px" color="inherit" />,
+    component: Profile,
+  },
+  {
+    name: "Latency",
+    layout: "/admin",
+    path: "/profile",
+    icon: <Icon as={MdPerson} width="20px" height="20px" color="inherit" />,
+    component: Profile,
+  },
+  {
+    name: "Throughtput",
+    layout: "/admin",
+    path: "/profile",
+    icon: <Icon as={MdPerson} width="20px" height="20px" color="inherit" />,
+    component: Profile,
+  },
+  {
+    name: "Mass export",
+    layout: "/admin",
+    path: "/profile",
+    icon: <Icon as={MdPerson} width="20px" height="20px" color="inherit" />,
+    component: Profile,
+  },
+  {
+    name: "Sample",
+    layout: "/admin",
+    path: "/profile",
+    icon: <Icon as={MdPerson} width="20px" height="20px" color="inherit" />,
+    component: Profile,
   },
   {
     name: "Profile",
     layout: "/admin",
     path: "/profile",
-    icon: <Icon as={MdPerson} width='20px' height='20px' color='inherit' />,
+    icon: <Icon as={MdPerson} width="20px" height="20px" color="inherit" />,
     component: Profile,
   },
   {
     name: "Sign In",
     layout: "/auth",
     path: "/sign-in",
-    icon: <Icon as={MdLock} width='20px' height='20px' color='inherit' />,
+    icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
     component: SignInCentered,
   },
   {
-    name: "RTL Admin",
+    name: "Map View",
     layout: "/rtl",
     path: "/rtl-default",
-    icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
+    icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
+    component: RTL,
+  },
+  {
+    name: "Intellegent maps",
+    layout: "/rtl",
+    path: "/rtl-default",
+    icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
+    component: RTL,
+  },
+  {
+    name: "Support",
+    layout: "/rtl",
+    path: "/rtl-default",
+    icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
     component: RTL,
   },
 ];
